baf1dde87f4a7440b67097976148d808ec89d825
alfio pietrantuono <alfio@localhost.localdomain>
2025-02-12 18:26:46 +0100

    Aggiunta route tasks.index

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// visualizzazione dei task
+Route::get('/tasks', [TaskController::class, 'index'])->name("tasks.index");
